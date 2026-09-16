54e40f377b949913ab43ff31de170ea0a26f679d
Sayali Pawar <139886338+MeSayali@users.noreply.github.com>
2026-09-16 12:44:04 +0530

    Time: 0 ms (100%), Space: 13.4 MB (44.02%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();//col ele
+        int n=matrix[0].size();//row ele
+
+        int left=0;
+        int right=m*n-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int row=mid/n; //quotient
+            int col=mid%n; //remiander
+            
+            if(matrix[row][col]==target){
+               return true;
+            }
+            else if(matrix[row][col]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return false;
+    }
+};
